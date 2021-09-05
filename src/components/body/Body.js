40a5af0bb7eb8cbb333dba6e81382cbbd9a6cb3f
Gatij Taranekar <gatij.taranekar@gmail.com>
2021-09-05 23:25:49 +0530

    added drag and drop feature

--- src/components/body/Body.js
+++ src/components/body/Body.js
@@ -1,65 +1,102 @@
-import React from 'react'
+import React, { useState } from 'react'
+import './Body.css'
 import Note from '../note/Note'
 
 const Body = () => {
-    const data = [
+    const [notes, setNotes] = useState([
         {
-            id: 1,
+            id: "aa",
             title: 'Michael Scott',
             content: ' is accomplished but he gets to be the center of attention.'
         },
         {
-            id: 2,
+            id: "bb",
             title: 'Dwight Schrute',
             content: 'te about paper and is constantly trying to impress Michael. Dwight is also the '
         },
         {
-            id: 3,
+            id: "cc",
             title: 'Jim Halpert',
             content: 'Jim is ack attitude and abili Pam Beasleper saspntertaining hntertain Pam. Several videos on the site demonstrate how Jims work ethic is reflected by the incentive structure the company has in place.'
         },
         {
-            id: 4,
+            id: "dd",
             title: 'Pam Beasley',
             content: 'Pam is the secretary at Dunder Mifflin and tries to keep Michael somewhat focused on work and being productive. Pam is unaware of Jims romantic interest in her during the early seasons of the show. Pam eventually becomes the office manager who demonstrates an understanding of budgets and cost constraints.'
         },
         {
-            id: 5,
+            id: "ee",
             title: 'Stanley Hudson',
             content: 'Stanley is a fairly successful salesman who resents spending time in an office where he typically is not productive. Stanley tolerates Michael long and typically pointless conference room meetings by spending his time completing crossword puzzles.'
         },
         {
-            id: 6,
+            id: "ff",
             title: 'Phyllis Lapin',
             content: 'back and quiet she is very protective of her position in the company and her sales commissions.'
         },
         {
-            id: 7,
+            id: "gg",
             title: 'Stanley Hudson',
             content: 'Stanley is a fairly successful salesman who resents spending time in an office where he typically is not productive. Stanley tolerates Michael long and typically pointless conference room meetings by spending his time completing crossword puzzles.'
         },
         {
-            id: 8,
+            id: "hh",
             title: 'Stanley Hudson',
             content: 'Stanley is a fairly successful salesman who resents spending time in an office where he typically is not productive. Stanley tolerates Michael long and typically pointless conference room meetings by spending his time completing crossword puzzles.'
         },
         {
-            id: 9,
+            id: "ii",
             title: 'Stanley Hudson',
             content: 'Stanley is a fairly successful salesman who resents spending time in an office where he typically is not productive. Stanley tolerates Michael long and typically pointless conference room meetings by spending his time completing crossword puzzles.'
         },
-    ]
+    ])
+
+    const onDragOver = (event) => {
+        event.preventDefault();
+    }
+
+    const findIndex = (arr, idx) => {
+        for (let i = 0; i < arr.length; i++) {
+            if (arr[i].id === idx) {
+                return i
+            }
+        }
+        return -1
+    }
+
+    const swap = (arr, src, target) => {
+        var tmp = arr[src];
+        arr[src] = arr[target];
+        arr[target] = tmp;
+
+        return arr
+    }
+
+
+    const onDrop = (event) => {
+        let noteSourceId = event.dataTransfer.getData("note_id");
+        let noteTargetId = event.target.id
+
+        let data = [...notes]
+
+        let src_idx = findIndex(data, noteSourceId);
+        let target_idx = findIndex(data, noteTargetId);
+        swap(data, src_idx, target_idx);
+        setNotes(data)
+
+    }
 
     return (
-        <div style={{ margin: "2%" }}>
+        <div className="note-container" onDragOver={(event) => onDragOver(event)}
+            onDrop={(event) => onDrop(event)}>
             {
-                data.map((d) => {
+                notes.map((n) => {
                     return (
-                        <Note title={d.title} content={d.content} key={d.id} />
+                        <Note title={n.title} content={n.content} key={n.id} id={n.id} />
                     )
                 })
             }
-        </div>
+        </div >
     )
 }
 
--- src/components/note/Note.js
+++ src/components/note/Note.js
@@ -1,12 +1,21 @@
 import React from 'react'
 import './Note.css'
 
-const Note = ({ title, content }) => {
+const Note = ({ title, content, id }) => {
+
+    const onDragStart = (event, note_id) => {
+        console.log('dragstart on div: ', note_id);
+        event.dataTransfer.setData("note_id", note_id);
+    }
+
     return (
-        <div className="note">
-            <h1>{title}</h1>
-            <p>{content}</p>
+
+        <div className="note" draggable onDragStart={(event) => onDragStart(event, id)} id={id}>
+            <h1 id={id}>{title}</h1>
+            <p id={id}>{content}</p>
         </div>
+
+
     )
 }
 
