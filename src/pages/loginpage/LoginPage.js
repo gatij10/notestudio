7b67932a37ab1fcb02760eaa16e15bd68345b323
Gatij Taranekar <gatij.taranekar@gmail.com>
2021-09-08 18:17:48 +0530

    login page styling done

--- src/pages/loginpage/LoginPage.js
+++ src/pages/loginpage/LoginPage.js
@@ -1,10 +1,17 @@
 import React from 'react'
+import taking_notes from './assets/taking_notes.png'
 import LoginForm from '../../components/loginform/LoginForm'
 
 const LoginPage = () => {
     return (
         <div>
-            <LoginForm />
+            <img src={taking_notes} alt="test" style={{
+                width: "500px",
+                position: 'absolute',
+                marginRight: "10%",
+                right: '0',
+            }} />
+            <LoginForm />s
         </div>
     )
 }
